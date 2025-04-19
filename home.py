262c4641d2ad7fb14fa2390b80862372fbd286c6
Vitor <vitoremanueldev2@gmail.com>
2025-04-18 22:57:16 -0300

    notebook disponivel para acesso

--- home.py
+++ home.py
@@ -1,15 +1,31 @@
 import streamlit as st
+import os
+import platform
+import subprocess
+
+html_file = "eda_notebook.html"
 
 st.title("Análise Exploratória de Dados - Campanha de Marketing")
 
 st.write("""<h2 style='font-size: 24px;'>Neste projeto busquei fazer uma análise exploratória dos dados,
          utilizando unicamente estatística descritiva para entender o perfil dos clientes e seus comportamentos em 
-         relação as campanhas de marking promovidas. Também tentei indentificar oportunidades para melhorar as campanhas,
+         relação às campanhas de marketing promovidas. Também tentei identificar oportunidades para melhorar as campanhas,
          dado que a grande maioria dos nossos clientes não participam delas. Assim, fiz a limpeza e preparação dos dados.
           Após isso, explorei todas as variáveis para entender o padrão de compra dos usuários e fazer recomendações baseadas nos insights
          que obtive ao explorar o dataset.</h2>""", unsafe_allow_html=True)
 
-st.write("""<h2 style='font-size: 24px;'>Todo o projeto foi feito utilizando python e suas bibliotecas, principalmente Pandas, Seaborn e matplotlib.
-          Após pensar em como apresentar os dados, resolvi utilizar a biblioteca streamlit para fazer essa página contendo todos os insights,
-         de forma rápida e visual sem precisar ficar olhando o código no notebook, porém ele está disponivel abaixo para possiveis duvidas
+st.write("""<h2 style='font-size: 24px;'>Todo o projeto foi feito utilizando Python e suas bibliotecas, principalmente Pandas, Seaborn e Matplotlib.
+          Após pensar em como apresentar os dados, resolvi utilizar a biblioteca Streamlit para fazer essa página contendo todos os insights,
+         de forma rápida e visual sem precisar ficar olhando o código no notebook. Porém, ele está disponível abaixo para possíveis dúvidas
          ou curiosidades sobre a análise.</h2>""", unsafe_allow_html=True)
+
+
+html_path = os.path.abspath(html_file)
+
+if st.button("Abrir Notebook"):
+    if platform.system() == "Windows":
+        os.startfile(html_path)
+    elif platform.system() == "Darwin":
+        subprocess.Popen(["open", html_path])
+    else:
+        subprocess.Popen(["xdg-open", html_path])
